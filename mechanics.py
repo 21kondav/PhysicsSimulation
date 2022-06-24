28369ada870c604670e5e20e47d09cb295e7c1e6
David Kong <21kondav@gmail.com>
2022-06-24 10:32:22 -0400

    Added comments and cleaned code up

--- mechanics.py
+++ mechanics.py
@@ -2,82 +2,114 @@ import numpy as np
 from sympy.physics.mechanics import *
 from sympy import *
 from sympy.physics.vector import *
-#creating vectors and operating
-m = Symbol('m')
-po = Point('po')
-pa = Particle('pa',po, m)
-#Building calculus methods
-system = {} #a dictionary containing particles and their distance from the center
 
-class Kinematics:
-    def __init__(self, N, part, eqMotion):
+# most fundamental to any classical system
+m = Symbol('m')  # mass for future use
+po = Point('po')  # point
+pa = Particle('pa', po, m)  # particle
+system = {}  # a dictionary containing particles and their distance from the center
+
+# assumptions for kinematics:
+#acceleration is constant
+#time is continuous and symmetric
+#space is uniform and symmetric
+# all events are deterministic
+# all positions and velocities are absolute
+
+
+class Kinematics:  # the backbone for all things kinematic, i.e. when an absolute position, velocity, or acceleration is known
+    def __init__(self, N, part, eqMotion):  # creates a kinematic scenario
+        # defines a reference for which an object is present in
         N = ReferenceFrame(N)
-        self.part = part
+        self.part = part  # possibly unecesary ------------
         self.eqMotion = eqMotion
-    def createEqMot(self, r, v, a):
-        self.eqMotion = {'r': r, 'v': v, 'a': a}
 
-class Equation: #creates equation and methods to act on
+    # a set of a equations which defines the motion of any object; the goal for any classical solution to a problem
+    def createEqMot(self, r, v, a):
+        self.eqMotion = {'Position': r, 'Velocity': v, 'Acceleration': a}
+
+    def getEqMot(self, value):  # Param value: "Position", "Velocity", "Acceleration"
+        return self.eqMotion.get(value)
+
+    def addToSystem(self):  # adds to the dictionary system
+        pos = self.getEqMot("Position")
+        obj = self
+        obLocation = {obj: pos}
+        system.update(obLocation)
+
+
+class Equation:  # creates equation and methods to act on
     def __init__(self, eq):
+        # defines common symbols (may need updated)
         x, y, z, t = symbols('x y z t')
         expr = ''
+        # replaces characters with symbols
         for char in eq:
-            if(char=='x'):
+            if(char == 'x'):
                 expr = expr + x
-            elif(char=='y'):
+            elif(char == 'y'):
                 expr = expr + y
-            elif(char=='z'):
+            elif(char == 'z'):
                 expr = expr + z
-            elif(char=='t'):
+            elif(char == 't'):
                 expr = expr + t
             else:
                 expr = expr + char
-        self.sympify(expr)
-    def fPrime(self, var):
-        self = sympify(self)
+        self.sympify(expr)  # allows sympy to parse the expression
+
+    def fPrime(self, var):  # derivative of self with respect to var
+        self = sympify(self)  # possibly unecesary ------------
         var = symbols(var)
         diff(self, var)
-    def integral(self, var):
-        self = sympify(self)
+
+    def integral(self, var):  # derivative of self with respect to var
+        self = sympify(self)  # possibly unecesary ------------
         var = symbols(var)
         integrate(self, var)
 
-    def numVar(self, var, a):
+    def numVar(self, var, a):  # substitutes a number for a variable (for solving numerical problems)
         return self.subs(var, a)
 
+
 class Vector:
-    def __init__(self, x, y, z):
+    def __init__(self, x, y, z):  # creates a vector
         N = ReferenceFrame('N')
         self.x = x
         self.y = y
         self.z = z
         self.vect = [x, y, z]
-    def scale(self, comp, n): #scale component
+
+    def scale(self, comp, n):  # scale component
         if comp.equals('x'):
-            self.x = self.x *n
+            self.x = self.x * n
         if comp.equals('y'):
-            self.y = self.y *n
+            self.y = self.y * n
         if comp.equals('z'):
-            self.z = self.z *n
+            self.z = self.z * n
         if comp.equals('r'):
             self.x = self.x * n
-            self.y = self.y *n
-            self.z = self.z *n 
-    def getVect(self):
+            self.y = self.y * n
+            self.z = self.z * n
+
+    def getVect(self):  # returns vector
         return self.vect
-    def cross(self, vect2):
+
+    def cross(self, vect2):  # cross product between self and another vector
         x = self.vect[1]*vect2.vect[2] - self.vect[2]*vect2.vect[1]
         y = self.vect[2]*vect2.vect[0]-self.vect[0]*vect2[2]
         z = self.vect[0]*vect2.vect[3]-self.vect[3]*vect2[0]
         crossed = Vector(x, y, z)
         return crossed
-    def dot(self, vect2):
-        d = self.vect[0] * vect2.vect[0] + self.vect[1] *vect2.vect[1] +self.vect[2] *vect2.vect[2]
+
+    def dot(self, vect2):  # dot product between self and another vector
+        d = self.vect[0] * vect2.vect[0] + self.vect[1] * \
+            vect2.vect[1] + self.vect[2] * vect2.vect[2]
         return d
-    def addVect(self, vect2):
+
+    def addVect(self, vect2):  # addition between self and another vector
         x = self.x + vect2.x
         y = self.y + vect2.y
         z = self.z + vect2.z
         addVect = Vector(x, y, z)
         return addVect
-#Calculus on vectors
+# Calculus on vectors
